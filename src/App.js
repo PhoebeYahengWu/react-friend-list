1a6bbc0a8fb1468a6bbf34ae35e1d09e9e8c76e3
wuyaheng <wuyaheng2016@gmail.com>
2020-06-13 20:34:27 -0400

    create remove function

--- src/App.js
+++ src/App.js
@@ -8,10 +8,16 @@ class App extends Component {
   
   state = {friends: friends};
 
+  removeFriend = id => {
+    const filteredFriends = this.state.friends.filter(friend => !(id === friend.id))
+
+    this.setState({friends: filteredFriends})
+  }
+
   render() {
     return (
       <Wrapper>
-        <h1 className="title">Friends List</h1>
+        <h1 className="title">Bestie List</h1>
         {this.state.friends.map(friend => (
         <FriendCard
           key={friend.id}
@@ -20,6 +26,7 @@ class App extends Component {
           image={friend.image}
           occupation={friend.occupation}
           location={friend.location}
+          delete={this.removeFriend}
         />
       ))}
       </Wrapper>
