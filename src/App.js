0ac3acf043794bcc5f93dbc08636965c64caa8a9
wuyaheng <wuyaheng2016@gmail.com>
2020-06-13 20:19:39 -0400

    refactor friends list

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+import "./App.css";
+
+class App extends Component {
+  
+  state = {friends: friends};
+
+  render() {
+    return (
+      <Wrapper>
+        <h1 className="title">Friends List</h1>
+        {this.state.friends.map(friend => (
+        <FriendCard
+          key={friend.id}
+          id={friend.id}
+          name={friend.name}
+          image={friend.image}
+          occupation={friend.occupation}
+          location={friend.location}
+        />
+      ))}
+      </Wrapper>
+    );
+  }
+
+}
+
+export default App;
